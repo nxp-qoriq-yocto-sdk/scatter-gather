74a5302104ca93b1e259b4755c84d8e259b38554
Cosmin Paraschiv <cosmin.paraschiv@freescale.com>
2014-05-28 15:57:30 +0300

    Add copyright and license information
    Signed-off-by: Cosmin Paraschiv <cosmin.paraschiv@freescale.com>

--- scatter-gather.c
+++ scatter-gather.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2014 Freescale Semiconductor, Inc.
+ * All rights reserved.
+ *
+ * This software may be distributed under the terms of the
+ * GNU General Public License ("GPL") as published by the Free Software
+ * Foundation, either version 2 of that License or (at your option) any
+ * later version.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Freescale Semiconductor ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Freescale Semiconductor BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <linux/module.h>
 #include <linux/debugfs.h>
 #include <linux/mm.h>
@@ -23,7 +44,7 @@ static const struct file_operations sgt_fops = {
 };
 
 MODULE_DESCRIPTION("Scatter-gather logic for multiple tables");
-MODULE_AUTHOR("Cosmin Paraschiv <cosmin.paraschiv@freescale.com>");
+MODULE_AUTHOR("freescale.com");
 MODULE_LICENSE("GPL");
 
 /**
